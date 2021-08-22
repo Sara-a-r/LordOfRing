16e3d2d664c9d042f2ce658fd5a25ef235a5e831
Sara Ardito <sarah02_ardito@hotmail.it>
2021-08-22 18:44:57 +0200

    modified triplets selection alghoritm

--- LordOfRings/ringfit.py
+++ LordOfRings/ringfit.py
@@ -4,8 +4,6 @@ This module fit multiple circle in sparse matrix
 
 import numpy as np
 
-maxhits = 64 # closer multiple of two to 84
-
 def load_data(list_events):
     """
     Parameters
@@ -25,18 +23,18 @@ def load_data(list_events):
     for circle_name in list_events:
         circle = np.loadtxt('data/'+circle_name)
         coord = np.argwhere(circle!=0)
-        X = coord[:, 1] + 1
-        Y = coord[:, 0] + 1
+        X = coord[:, 0] + 1
+        Y = coord[:, 1] + 1
         dict_data[circle_name] = [X, Y]
     return dict_data
 
 
-def init_triplets(dict_events, t=1):
+def init_triplets(dict_events, maxhits = 64, t=10):
     """
     init_triplets gives three array: the first contains the index of border hits
-    in the sparse matrix of each event, the second contains the x coordinates of
-    hits for all events in a single array, the third contains the y coordinates
-    as before.
+    in the sparse matrix of each event whose reciprocal distance is greater then
+    t,the second contains the x coordinates of hits for all events in a single 
+    array, the third contains the y coordinates as before.
 
     Parameters
     ----------
@@ -44,6 +42,10 @@ def init_triplets(dict_events, t=1):
         Dictionary whose keys are the name of .txt files and whose values are  
         the x, y coordinates of the relative event in a list.
          This format is the output of the function LordOfRing.ringfit.load_data.
+    
+    maxhits : int
+        Maximum number of points per event (i.e. the maximum number of ones in
+        each sparse matrix).
 
     t : float
         Treshold value for the selection of the triplets, it defines the minimum 
@@ -51,7 +53,7 @@ def init_triplets(dict_events, t=1):
 
     Returns
     -------
-    ( 1d numpy-array, 1d numpy-array, 1d numpy-array ) [float, float, float]
+    ( 1d numpy-array, 1d numpy-array, 1d numpy-array ) [int, float, float]
         The triplet and the coordinates in a tuple  (triplet, X, Y).
 
     """
@@ -70,19 +72,31 @@ def init_triplets(dict_events, t=1):
     X = np.zeros((nevents, maxhits))
     Y = np.zeros((nevents, maxhits))
 
-    def d3(x, y, z, t):
+    def d3(x, y, z, t, max):
         # this function return boolean array of len = 3:
         # - True if the d[i, j] > t
         # - False otherwise. 
         dxy = np.linalg.norm(x-y)
         dyz = np.linalg.norm(y-z)
         dxz = np.linalg.norm(x-z)
-        return np.array([(dxy>t), (dyz>t), (dxz>t)])
+        if max: # for xmax and ymax
+            if (dyz>t): # if the element y respects the threshold check only xy
+                return np.array([(dxy>t) and (dxz>t), True, True]).astype(bool)
+            if (dxz>t): # if the element x respects the threshold check only xy
+                return np.array([True, (dxy>t) and (dyz>t), True]).astype(bool)
+            # if both x and y don't respect the t.. check both of them (dxz and dyz)
+            return np.array([(dxz>t), (dyz>t), True]).astype(bool)
+        else: # for xmin and ymin 
+            if (dxy>t): # if the element y respects the threshold check only yz
+                return np.array([True, True, (dyz>t) and (dxz>t)]).astype(bool)
+            if (dxz>t): # if the element z respects the threshold check only yz
+                return np.array([True, (dyz>t) and (dxy>t), True]).astype(bool)
+
+            return np.array([True, (dxy>t), (dxz>t)]).astype(bool)
 
     # Fill one event at time in the triplet matrix and X, Y matrix
     for i, (xi, yi) in enumerate(dict_events.values()):
         #xi, yi = rf.get_coord(circle) # Get the sorted coord of event
-
         X[i, :len(xi)] = xi
         Y[i, :len(yi)] = yi
 
@@ -102,11 +116,11 @@ def init_triplets(dict_events, t=1):
             boolarrayy = np.ones(len(idx_sy)).astype(bool)
 
             # Evaluate the reciprocal distance of the points in the same triplet
-            bminx = d3(*xsortv[:3], t)
-            bmaxx = d3(*xsortv[-3:], t)
-            bminy = d3(*ysortv[:3], t)
-            bmaxy = d3(*ysortv[-3:], t)
-            
+            bminx = d3(*xsortv[:3], t, max = False)
+            bmaxx = d3(*xsortv[-3:], t, max = True)
+            bminy = d3(*ysortv[:3], t, max = False)
+            bmaxy = d3(*ysortv[-3:], t, max = True)
+
             # if all the values in all the 4 bool array are true
             if bminx.all() and bmaxx.all() and bminy.all() and bmaxy.all():
                 # we've the right triplets and we exit
@@ -115,25 +129,34 @@ def init_triplets(dict_events, t=1):
                 # sobstitute in boolarray the first/last three bool elements
                 boolarrayx[:3] = bminx
                 boolarrayx[-3:] = bmaxx
-                
-                # delete (with the mask boolarrayx) the elements that not 
-                # satisfy the contition
-                idx_sx = idx_sx[boolarrayx]
-                xsortv = xsortv[boolarrayx]
-
-                # same for the y index ordered array
                 boolarrayy[:3] = bminy
                 boolarrayy[-3:] = bmaxy
-                idx_sy = idx_sy[boolarrayy]
-                ysortv = ysortv[boolarrayy]
+                
+                if (np.sum(boolarrayx) < 3) or (np.sum(boolarrayy) < 3): # if the element are finished
+                    boolcheck = False # get out with this index
+                else:
+                    # delete (with the mask boolarrayx) the elements that not 
+                    # satisfy the contition
+                    idx_sx = idx_sx[boolarrayx]
+                    xsortv = xsortv[boolarrayx]
+                    
+                    # same for the y index ordered array
+                    idx_sy = idx_sy[boolarrayy]
+                    ysortv = ysortv[boolarrayy]
+                
+        # sorting in the single triplet for the opposite variable (for ptolemy)
+        iy_sort_xmax = np.argsort(xsortv[-3:, 1]) # index sorted based on y for xmax
+        iy_sort_xmin = np.argsort(xsortv[:3, 1])  # ...
+        ix_sort_ymax = np.argsort(ysortv[-3:, 0])
+        ix_sort_ymin = np.argsort(ysortv[:3, 0])
         
         # Fill the triplet with maximum and minumum relative to the event
-        triplet[i,  :3]  = idx_sx[-3:] #XMAX
-        triplet[i, 3:6]  = idx_sx[ :3] #XMIN
-        triplet[i, 6:9]  = idx_sy[-3:] #YMAX
-        triplet[i, 9: ]  = idx_sy[ :3] #YMIN
+        triplet[i,  :3]  = idx_sx[-3:][iy_sort_xmax] + i * maxhits #XMAX
+        triplet[i, 3:6]  = idx_sx[ :3][iy_sort_xmin] + i * maxhits #XMIN
+        triplet[i, 6:9]  = idx_sy[-3:][ix_sort_ymax] + i * maxhits #YMAX
+        triplet[i, 9: ]  = idx_sy[ :3][ix_sort_ymin] + i * maxhits #YMIN
 
     triplet = triplet.flatten()
     X = X.flatten()
     Y = Y.flatten()
-    return triplet, X, Y
+    return triplet.astype(int), X, Y
