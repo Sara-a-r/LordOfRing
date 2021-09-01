cc6184b459011ca9f5369b240a114802abe75bdc
dodogabrie <dodogabrie97@live.it>
2021-09-01 02:51:30 +0200

    Added extension for syntax highilighting in notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,8 @@ release = '0.0.0'
 extensions = [
         'sphinx.ext.autodoc', 
         'numpydoc',
-        "nbsphinx"
+        'nbsphinx',
+        'IPython.sphinxext.ipython_console_highlighting'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
